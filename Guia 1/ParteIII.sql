f9fef7fd5a17bf879739c924d556afd5f3753e22
@Garay01 <garayalvarado14@gmail.com>
2020-07-25 11:50:12 -0600

    Finaliza detalles en parteIII

--- Guia 1/ParteIII.sql
+++ Guia 1/ParteIII.sql
@@ -1,5 +1,4 @@
 -- Parte III
-
 USE Control_de_libros_CG181933;
 -- Creando tabla de autores_espana
 
@@ -11,13 +10,8 @@ create table autores_espana (
 
 -- Insertando datos en tabla
 insert into autores_espana select Codigo, Nombres, Apellidos from Autor where Nacionalidad = 'Española';
-use library;
 
--- Creando vista
-DROP VIEW copia_libro
-create view copia_libro as
-select * from (select cp.isbn as ISBN, copy_no as 'Copia', case when on_loan = 'Y'  then 'Si' when on_loan = 'N' then 'No' else 'No' end as 'Prestado', title as 'Título', translation as 'Traducción', cover as 'Forro' from copy cp inner join title tt on cp.title_no = tt.title_no inner join item it on cp.isbn = it.isbn) as intermedium where ISBN = 1 or ISBN = 500 or ISBN = 1000;
 
-select * from copia_libro
-select * from (select cp.isbn as ISBN, copy_no as 'Copia', case when on_loan = 'Y'  then 'Si' when on_loan = 'N' then 'No' else 'No' end as 'Prestado', title as 'Título', translation as 'Traducción', cover as 'Forro' from copy cp inner join title tt on cp.title_no = tt.title_no inner join item it on cp.isbn = it.isbn) as intermedium where ISBN = 1 or ISBN = 500 or ISBN = 1000;
-
+-- usando view copia_libro (vista creada en Vista_copia_libro.sql)
+USE library;
+select * from copia_libro order by ISBN;
